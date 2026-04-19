b4126baf50851831ed7db9e8d4d883ea80a649c8
faizunroyalbengalai <faizun.royalbengalai@gmail.com>
2026-04-20 01:00:27 +0600

    Init java project: src/test/java/com/example/springecommerce/SpringecommerceApplicationTests.java

---
+++ src/test/java/com/example/springecommerce/SpringecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringecommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
